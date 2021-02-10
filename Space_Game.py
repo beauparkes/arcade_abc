658a873d9d10150b2a114d80adf0f407eeca430e
Beau <beau.parkes@me.com>
2021-02-11 00:02:48 +1100

    added space game

---
+++ Space_Game.py
@@ -0,0 +1,11 @@
+import arcade
+import random
+
+SCREEN_WIDTH = 800
+SCREEN_HEIGHT = 600
+SCREEN_TITLE = "Arcade Space Shooter"
+SCALING = 2.0
+
+class SpaceShooter(arcade.window):
+    """Space Shooter side scroller game
+    Player starts on the 
